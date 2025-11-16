1c52157c79861c09e63bf09d5797b7687d96098f
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-11-16 18:44:20 +0530

    Change in urls

--- maria/common/functions/find_urls.py
+++ maria/common/functions/find_urls.py
@@ -2,27 +2,27 @@
 def find_be_url(environment=None):
     """
     Returns the url based on the environment
-    if environment is dev then return https://dev.myfairworx.com/api/
-    if environment is stage then return https://stage.myfairworx.com/api/
+    if environment is dev then return https://dev.com/api/
+    if environment is stage then return https://stage.com/api/
     if local then return http://127.0.0.1:8000/api/
     """
     if environment == "dev":
-        return "https://dev.myfairworx.com/api/"
+        return "https://dev.com/api/"
     elif environment == "stage":
-        return "https://stage.myfairworx.com/api/"
+        return "https://stage.com/api/"
     else:
         return "http://127.0.0.1:8000/api/"
     
 def find_fe_url(environment=None):
     """
     Returns the url based on the environment
-    if environment is dev then return https://dev.myfairworx.com/
-    if environment is stage then return https://stage.myfairworx.com/
+    if environment is dev then return https://dev.com/
+    if environment is stage then return https://stage.com/
     if local then return http://127.0.0.1:8000/
     """
     if environment == "dev":
-        return "https://dev.myfairworx.com/"
+        return "https://dev.com/"
     elif environment == "stage":
-        return "https://stage.myfairworx.com/"
+        return "https://stage.com/"
     else:
         return "http://127.0.0.1:8001/silk/"
