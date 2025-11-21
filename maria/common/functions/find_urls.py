271a33340e6fee94ec849ff26fc8d6679147b860
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-11-21 22:58:24 +0530

    feat: enhance URL functions and add query logging

--- maria/common/functions/find_urls.py
+++ maria/common/functions/find_urls.py
@@ -1,4 +1,9 @@
+import time
+import logging
+from django.db import connection, reset_queries
+from functools import wraps
 
+logger = logging.getLogger(__name__)
 def find_be_url(environment=None):
     """
     Returns the url based on the environment
@@ -26,3 +31,27 @@ def find_fe_url(environment=None):
         return "https://stage.com/"
     else:
         return "http://127.0.0.1:8001/silk/"
+    
+
+
+
+def log_query_count(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        reset_queries()
+        start_queries = len(connection.queries)
+        start_time = time.time()
+
+        result = func(*args, **kwargs)
+
+        end_time = time.time()
+        end_queries = len(connection.queries)
+        total_queries = end_queries - start_queries
+        execution_time = end_time - start_time
+
+        print(
+            f"Function `{func.__name__}` executed {total_queries} queries in {execution_time:.2f}s"
+        )
+
+        return result
+    return wrapper
--- maria/maria/settings.py
+++ maria/maria/settings.py
@@ -181,7 +181,7 @@ name=f"MYAPP"
 JAZZMIN_SETTINGS = {
     "site_brand": name,
     "site_title": name,
-    "site_logo": "images/logo.png",
+    "site_logo": "Information Technology.png",
     "site_header": f"{name} Admin",
     "welcome_sign": f"Welcome to the {name} Admin",
     "copyright": f"{name} Ltd ",
